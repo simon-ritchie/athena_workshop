150489381399b25f747414a949986cb5ec1ea8cf
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-18 08:51:20 +0900

    パーティションプロジェクションを使う形にDDLを調整。

--- ddl/login.sql
+++ ddl/login.sql
@@ -13,6 +13,11 @@ OUTPUTFORMAT
 LOCATION
   's3://<S3バケット名>/workshop/login'
 TBLPROPERTIES (
-  'has_encrypted_data'='false');
-
-MSCK REPAIR TABLE login;
+  'has_encrypted_data'='false',
+    'projection.enabled' = 'true',
+    'projection.dt.type' = 'date',
+    'projection.dt.range' = '2021-01-01,NOW',
+    'projection.dt.format' = 'yyyy-MM-dd',
+    'projection.dt.interval' = '1',
+    'projection.dt.interval.unit' = 'DAYS',
+    'storage.location.template' = 's3://<S3バケット名>/workshop/login/dt=${dt}');
