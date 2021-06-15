0b6a316024224c4b56231bf60a58bac8f2753ad2
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-16 08:49:48 +0900

    列結合のサンプルテーブル用のDDLのSQLファイルを追加。

---
+++ ddl/join_sample_right_table.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE `join_sample_right_table`(
+  `id` bigint, 
+  `attack` bigint)
+PARTITIONED BY ( 
+  `dt` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://<S3バケット名>/workshop/join_sample_right_table'
+TBLPROPERTIES (
+  'has_encrypted_data'='false',
+    'projection.enabled' = 'true',
+    'projection.dt.type' = 'date',
+    'projection.dt.range' = '2021-01-01,NOW',
+    'projection.dt.format' = 'yyyy-MM-dd',
+    'projection.dt.interval' = '1',
+    'projection.dt.interval.unit' = 'DAYS',
+    'storage.location.template' = 's3://<S3バケット名>/workshop/join_sample_right_table/dt=${dt}');
