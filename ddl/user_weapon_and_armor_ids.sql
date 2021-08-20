de7a0019ecaf2be71b9c15ea342622e7bd5c8e26
simon-ritchie <antisocial.sid2@gmail.com>
2021-08-20 09:00:10 +0900

    ユーザーの所持武器と防具のIDの配列のテーブルデータを追加。

---
+++ ddl/user_weapon_and_armor_ids.sql
@@ -0,0 +1,25 @@
+CREATE EXTERNAL TABLE `user_weapon_and_armor_ids`(
+  `user_id` bigint, 
+  `date` string, 
+  `device_type` int,
+  `weapon_ids` array<bigint>,
+  `armor_ids` array<bigint>)
+PARTITIONED BY ( 
+  `dt` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://<S3バケット名>/workshop/user_weapon_and_armor_ids'
+TBLPROPERTIES (
+  'has_encrypted_data'='false',
+    'projection.enabled' = 'true',
+    'projection.dt.type' = 'date',
+    'projection.dt.range' = '2021-01-01,NOW',
+    'projection.dt.format' = 'yyyy-MM-dd',
+    'projection.dt.interval' = '1',
+    'projection.dt.interval.unit' = 'DAYS',
+    'storage.location.template' = 's3://<S3バケット名>/workshop/user_weapon_and_armor_ids/dt=${dt}');
