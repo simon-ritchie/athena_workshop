51f52a70a25a9953286c2a28f0cd2cc581bc1c85
simon-ritchie <antisocial.sid2@gmail.com>
2021-08-26 08:52:46 +0900

    クエスト（バトル）時のパーティーの戦闘力データのテーブルデータとDDLファイルを追加。

---
+++ ddl/battle_party_power.sql
@@ -0,0 +1,25 @@
+CREATE EXTERNAL TABLE `battle_party_power`(
+  `time` string,
+  `device_type` int,
+  `quest_id` bigint,
+  `user_ids` array<bigint>,
+  `powers` array<bigint>)
+PARTITIONED BY ( 
+  `dt` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://<S3バケット名>/workshop/battle_party_power'
+TBLPROPERTIES (
+  'has_encrypted_data'='false',
+    'projection.enabled' = 'true',
+    'projection.dt.type' = 'date',
+    'projection.dt.range' = '2021-01-01,NOW',
+    'projection.dt.format' = 'yyyy-MM-dd',
+    'projection.dt.interval' = '1',
+    'projection.dt.interval.unit' = 'DAYS',
+    'storage.location.template' = 's3://<S3バケット名>/workshop/battle_party_power/dt=${dt}');
