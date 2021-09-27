7e62699f8935f1b705f17a3166bcb86734531742
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-28 08:52:20 +0900

    JSONの文字列のカラムを持つテーブルのDDLを追加。

---
+++ ddl/user_weapon_json_str.sql
@@ -0,0 +1,24 @@
+CREATE EXTERNAL TABLE `user_weapon_json_str`(
+  `user_id` bigint,
+  `date` string,
+  `device_type` int,
+  `weapons` string)
+PARTITIONED BY ( 
+  `dt` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://simonritchie.athena.workshop/workshop/user_weapon_json_str'
+TBLPROPERTIES (
+  'has_encrypted_data'='false',
+    'projection.enabled' = 'true',
+    'projection.dt.type' = 'date',
+    'projection.dt.range' = '2021-01-01,NOW',
+    'projection.dt.format' = 'yyyy-MM-dd',
+    'projection.dt.interval' = '1',
+    'projection.dt.interval.unit' = 'DAYS',
+    'storage.location.template' = 's3://simonritchie.athena.workshop/workshop/user_weapon_json_str/dt=${dt}');
